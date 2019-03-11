0d13776bebef93c019e2daf5069025730519ec7f
Bjørge Næss <bjoerge@gmail.com>
2019-03-11 19:27:01 +0100

    Fix wrong typings field

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {fileStore} from './fileStore'
 import {memoryStore} from './memoryStore'
-import {withStore} from './rx-vcr'
 import {ReplayOptions, VCRMode} from './types'
+import {withStore} from './vcr'
 
 export const vcr = (mode: VCRMode, options?: ReplayOptions & {filename?: string}) => {
   const store = options && options.filename ? fileStore(options.filename) : memoryStore()
