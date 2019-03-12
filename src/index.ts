02e29db12bd7217bbf9fc7cdb0ac2e789bf94b87
Bjørge Næss <bjoerge@gmail.com>
2019-03-12 17:26:12 +0100

    Add type parameter to main export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import {MonoTypeOperatorFunction} from 'rxjs'
 import {fileStore} from './fileStore'
 import {memoryStore} from './memoryStore'
 import {ReplayOptions, VCRMode} from './types'
 import {withStore} from './vcr'
 
-export const vcr = (mode: VCRMode, options?: ReplayOptions & {filename?: string}) => {
-  const store = options && options.filename ? fileStore(options.filename) : memoryStore()
+export const vcr = <T>(
+  mode: VCRMode,
+  options?: ReplayOptions & {filename?: string},
+): MonoTypeOperatorFunction<T> => {
+  const store = options && options.filename ? fileStore<T>(options.filename) : memoryStore<T>()
   return withStore(store)(mode, options)
 }
